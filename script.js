6d1fa3507a3c5705c5c349db424baf79b93ad37d
Mahfuz Rahman <mahfuzrahman038@gmail.com>
2022-01-18 12:40:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var MENUITEMS = document.getElementById("MENUITEMS");
+MENUITEMS.style.maxHeight = "0px";
+if (MENUITEMS.style.maxHeight == "0px") {
+    MENUITEMS.style.maxHeight = "200px"
+}
+else {
+    MENUITEMS.style.maxHeight = "0px"
+}
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px";
+    Indicator.style.transform = "translateX(100px)";
+}
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px";
+    Indicator.style.transform = "translateX(0px";
+}
